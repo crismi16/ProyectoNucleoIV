3c011a454c02bd70244a99cb40a251030b85dca7
crismi16 <migueltulmo@yahoo.es>
2025-03-31 09:12:32 -0500

    styles2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CoursesService } from '../../services/courses/courses.service';
+import { Course } from '../../types/courses';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  featuredCourses: Course[] = [];
+
+  constructor(private coursesService: CoursesService, private router: Router) {}
+
+  ngOnInit() {
+    this.getCourses();
+  }
+
+  getCourses() {
+    this.coursesService.getCourses().subscribe(courses => {
+      this.featuredCourses = courses.slice(0, 3);
+    });
+  }
+
+  goToCourse(id: string) {
+    this.router.navigate(['/courses', id]);
+  }
 }
